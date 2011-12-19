06d078c7e4d64916097d46be174d4b70a2b854be
hughfdjackson <hughfdjackson@googlemail.com>
2011-12-19 14:09:03 +0000

    Added semi-colons

--- js/src/RCanvas.js
+++ js/src/RCanvas.js
@@ -1,18 +1,18 @@
 
 // At the moment, RCanvas expects the 2d context.  Open to change later
 function RCanvas(context){
-    this._context = context
+    this._context = context;
 }
 
 RCanvas.prototype = {
 
     handleMessage: function(msg){
-        var rpc = JSON.parse(msg)
+        var rpc = JSON.parse(msg);
 
         try {
 
             this._context[rpc.method]
-                .apply(this._context, rpc.params)
+                .apply(this._context, rpc.params);
             //... send success?
         
         } catch(e) {
